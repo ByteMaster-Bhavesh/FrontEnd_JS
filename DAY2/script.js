9eb22a5f891e3ac751b29b9c389970d992a1b372
ByteMaster-Bhavesh <somwanshibhavesh71@gmail.com>
2026-09-02 23:12:38 +0530

    DAY-2

---
+++ DAY2/script.js
@@ -0,0 +1,17 @@
+var addrm = document.querySelector(".add-rm-btn");
+var isstatus = document.querySelector("h2")
+
+check = 0;
+
+addrm.addEventListener("click",function(){
+    if (check == 0){
+        isstatus.innerHTML = 'Friends❤️',
+        isstatus.style.color = 'green',
+        check = 1;
+    }
+    else{
+        isstatus.innerHTML = 'Strenger😏',
+        isstatus.style.color = 'red',
+        check = 0;
+    }
+})
